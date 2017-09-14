ff50ac0e1510fed46aacff21e03e1848e9893608
Charles Wahome <thewahome662@gmail.com>
2017-09-14 15:09:13 +0300

    creating functions accepting many inputs

---
+++ FsharpOne/Processes.fsx
@@ -0,0 +1,15 @@
+let childAges = 2,4
+let addChildAges ages = 
+    let bob, sam = ages
+    bob+sam
+
+let addAges bob sam =
+    bob + sam
+
+let fnAddChildAges bob =
+    let addSamsAgeToBob sam = 
+        bob + sam
+    addSamsAgeToBob
+
+let areaOfTriangle height width =
+    0.5 * height * width
